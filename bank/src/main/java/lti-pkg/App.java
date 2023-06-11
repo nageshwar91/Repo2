ad5f3e78a88ae10df38a0ba4706380529de3184a
naga <naga@gmail.com>
2023-06-11 17:18:00 +0530

    commieted all files

---
+++ bank/src/main/java/lti-pkg/App.java
@@ -0,0 +1,13 @@
+package lti-pkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
